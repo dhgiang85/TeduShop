9ad25ad74daee9591bcd678dd64bbe2c29654036
HGIANG <dhgiang85@gmail.com>
2018-03-03 20:33:04 +0700

    commit migration and repository

---
+++ TeduShop.Data/Repositories/ProductReposity.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+using TeduShop.Data.Infrastructor;
+using TeduShop.Model.Models;
+using System.Linq;
+
+namespace TeduShop.Data.Repositories
+{
+    public interface IProductRepository
+    {
+        IEnumerable<Product> GetByAlias(string alias);
+    }
+    public class ProductReposity : RepositoryBase<Product>, IProductRepository
+    {
+        public ProductReposity(IDbFactory dbFactory) : base(dbFactory)
+        {
+        }
+
+        public IEnumerable<Product> GetByAlias(string alias)
+        {
+            return this.DbContext.Products.Where(x => x.Alias == alias);
+        }
+    }
+}
